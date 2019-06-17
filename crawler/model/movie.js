6a6f6340a38fae1c454f80ec71c587a735a2b53a
peiye <peiye@peiye.com>
2019-06-17 15:18:04 +0800

    push crawler

---
+++ crawler/model/movie.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+
+var movieFilePath = 'db/movie.json'
+
+const loadMovies = () => {
+    const content = fs.readFileSync(movieFilePath, 'utf8')
+    const ms = JSON.parse(content)
+    return ms
+}
+
+var m = {
+    data: loadMovies()
+}
+
+m.all = function() {
+    var ms = this.data
+    return ms
+}
+\
+
+module.exports = m
